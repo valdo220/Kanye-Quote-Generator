a3e742e7f5d1ab3ca6d35f561737d8234685bdda
Valdo <oswaldojr2200@gmail.com>
2025-03-28 00:19:12 -0500

    generates 5 quotes from kanye rest

---
+++ main.js
@@ -0,0 +1,52 @@
+import { BASE_URL } from "./secrets.js"
+
+async function randomQuoteGenerator(){
+
+const arrowUp = document.getElementById("arrow-up")
+const arrowDown = document.getElementById("arrow-down")
+const multiplier = document.getElementById("multiplier")
+// const quote = document.getElementById("quote")
+const quoteButton = document.getElementById("quoteButton")
+const quotes = document.getElementsByClassName("quote")
+let multiplierNumber = 1
+
+async function quoteText(){
+    for (let i = 0; i < quotes.length; i++){
+        quotes[i].hidden = false
+        console.log(quotes.hidden)
+        quotes[i].textContent = await retrieveQuote()
+    }
+}
+
+async function retrieveQuote(){
+    const data = await fetch("https://api.kanye.rest")
+    const quoteData = await data.json()
+    const quoteText = quoteData.quote
+    return quoteText
+}
+console.log(quotes)
+
+quoteButton.addEventListener('click', () => {
+    retrieveQuote()
+    quoteText()
+    console.log("quotes changed")
+})
+
+arrowUp.addEventListener('click', () => {
+    if (multiplierNumber < 5) {
+        multiplierNumber++
+        console.log("changed Number to " + multiplierNumber)
+        multiplier.textContent = "" + multiplierNumber
+    }
+})
+
+arrowDown.addEventListener('click', () => {
+    if (multiplierNumber > 1) {
+        multiplierNumber--
+        console.log("changed Number to " + multiplierNumber)
+        multiplier.textContent = "" + multiplierNumber
+    }
+})
+
+}
+randomQuoteGenerator()
